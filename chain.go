3770dd50a80968c8b5d0e3795e8b653d76d02bf2
Michael P. Soulier <msoulier@digitaltorque.ca>
2024-10-10 20:03:56 -0400

    Added cert file loading option to chain.

--- chain.go
+++ chain.go
@@ -5,6 +5,9 @@ import (
     "fmt"
     "flag"
     "crypto/tls"
+    "crypto/x509"
+    "encoding/pem"
+    "io/ioutil"
     "github.com/op/go-logging"
 )
 
@@ -13,6 +16,7 @@ var (
     noverify bool
     debug bool
     log *logging.Logger = nil
+    path string
 )
 
 func init() {
@@ -31,16 +35,36 @@ func init() {
     log = logging.MustGetLogger("chain")
 }
 
-func main() {
-    flag.StringVar(&connect, "connect", "", "address:port to connect to")
-    flag.BoolVar(&noverify, "noverify", false, "do not verify host cert")
-    flag.Parse()
-
-    if connect == "" {
-        log.Error("--connect is a required option")
-        os.Exit(1)
+func loadCerts(path string) ([]*x509.Certificate, error) {
+    certs := []*x509.Certificate{}
+    certbuf, err := ioutil.ReadFile(path)
+    if err != nil {
+        return certs, err
     }
+    roots := x509.NewCertPool()
+    ok := roots.AppendCertsFromPEM([]byte(certbuf))
+    if !ok {
+        log.Errorf("failed to parse certificate")
+        return certs, err
+    }
+    block, _ := pem.Decode([]byte(certbuf))
+	if block == nil {
+		log.Error("failed to parse certificate PEM")
+        return certs, err
+	}
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		log.Errorf("failed to parse certificate: %s", err)
+        return certs, err
+	}
 
+    certs = append(certs, cert)
+
+    return certs, nil
+}
+
+func connectHost(connect string, noverify bool) ([]*x509.Certificate, error) {
+    certs := []*x509.Certificate{}
     cfg := tls.Config{}
     if noverify {
         cfg.InsecureSkipVerify = true
@@ -48,10 +72,44 @@ func main() {
     conn, err := tls.Dial("tcp", connect, &cfg)
     if err != nil {
         log.Fatal("TLS connection failed: " + err.Error())
+        return certs, err
     }
     defer conn.Close()
 
-    certChain := conn.ConnectionState().PeerCertificates
+    certs = conn.ConnectionState().PeerCertificates
+    return certs, nil
+}
+
+func main() {
+    flag.StringVar(&connect, "connect", "", "address:port to connect to")
+    flag.StringVar(&path, "path", "", "path on disk to a PEM file to check")
+    flag.BoolVar(&noverify, "noverify", false, "do not verify host cert")
+    flag.Parse()
+
+    if connect == "" && path == "" {
+        log.Error("either --connect or --path are required options")
+        os.Exit(1)
+    }
+
+    certChain := []*x509.Certificate{}
+
+    var err error = nil
+    if connect != "" {
+        certChain, err = connectHost(connect, noverify)
+        if err != nil {
+            log.Errorf("connectHost: %s", err)
+            os.Exit(1)
+        }
+    } else if path != "" {
+        certChain, err = loadCerts(path)
+        if err != nil {
+            log.Errorf("loadCerts: %s", err)
+            os.Exit(1)
+        }
+    } else {
+        panic("we should not be here")
+    }
+
     for i, cert := range certChain {
         fmt.Println(i)
         fmt.Println("Issuer:", cert.Issuer)
